dfb2cc22dd2242dfe5230bf7b56b6e37790b4942
cqqccqc <cqqccqc@foxmail.com>
2015-10-02 23:25:24 +0800

    1.16

---
+++ 1.2.4.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+(define (square x)
+  (* x x))
+
+(fast-expt 2 2)
+(fast-expt 2 3)
