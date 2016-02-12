b478b20ba1a6fda2d051a2da7d7c15e5bb2054fc
cqqccqc <cqqccqc@foxmail.com>
2016-02-12 22:57:12 +0800

    exercise2.39

---
+++ exercise2.38.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+;; 满足结合律
