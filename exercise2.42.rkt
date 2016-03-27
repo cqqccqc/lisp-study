9e8047ebd3b7be5fccbb03567924de76d674ea2f
陈琦 <cqqccqc@foxmail.com>
2016-03-27 22:24:22 +0800

    2.3.2

--- exercise2.42.rkt
+++ exercise2.42.rkt
@@ -1,1 +1,23 @@
 #lang racket
+(define empty-board 0)
+(define safe? 1)
+(define flatmap 2)
+(define adjoin-position 3)
+(define (enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
