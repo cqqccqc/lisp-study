159d93a95cb88ea432658360ddec6863bab2f0e7
陈琦 <cqqccqc@foxmail.com>
2016-03-29 00:17:43 +0800

    exercise2/62

--- 2.3.3.rkt
+++ 2.3.3.rkt
@@ -16,3 +16,22 @@
                (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
 (intersection-set '(1 2 3 4) '(2 3 4 5))
+
+(define (element-of-set2? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set2? x (cdr set)))))
+
+(define (intersection-set2 set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set2 (cdr set1)
+                                        (cdr set2))))
+              ((< x1 x2)
+               (intersection-set2 (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set2 set1 (cdr set2)))))))
