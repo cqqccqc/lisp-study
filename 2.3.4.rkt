ccc4ecc61216af279050b6e7c1ac2a288a02d0f8
陈琦 <cqqccqc@foxmail.com>
2016-03-30 22:52:59 +0800

    2.3.4 init

--- 2.3.4.rkt
+++ 2.3.4.rkt
@@ -1,1 +1,7 @@
 #lang racket
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
