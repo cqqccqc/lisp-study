02997274e925a963eee3f42b3fa06bf9c50e6890
陈琦 <cqqccqc@foxmail.com>
2016-04-24 23:14:01 +0800

    exercise 3.2

--- exercise3.2.rkt
+++ exercise3.2.rkt
@@ -3,7 +3,7 @@
   (let ((count 0))
     (lambda (x)
       (cond ((eq? x 'reset-count) (set! count 0))
-            ((eq? x 'how-many-calls?) (count))
+            ((eq? x 'how-many-calls?) count)
             (else (begin (set! count (+ count 1))) (f x))))))
 
 (define s (make-monitored sqrt))
