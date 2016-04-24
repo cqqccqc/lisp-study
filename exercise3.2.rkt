43d30065161961145bbc38862178ef57c62b68a7
陈琦 <cqqccqc@foxmail.com>
2016-04-24 23:07:14 +0800

    exercise3.2

---
+++ exercise3.2.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(define (make-monitored f)
+  (let ((count 0))
+    (lambda (x)
+      (cond ((eq? x 'reset-count) (set! count 0))
+            ((eq? x 'how-many-calls?) (count))
+            (else (begin (set! count (+ count 1))) (f x))))))
+
+(define s (make-monitored sqrt))
+(s 100)
+(s 'how-many-calls?)
