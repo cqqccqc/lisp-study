96aa34b7e45c215676fac7542b6077059f33a2a3
陈琦 <cqqccqc@foxmail.com>
2016-04-27 23:11:15 +0800

    exercise3.3

---
+++ exercise3.3.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(define (make-account balance secret-password)
+  (define (withdraw amount)  
+    ((if (>= balance amount)
+         (begin (set! balance (- balance amount))
+                balance)
+         "Insufficient funds")))
+  (define (deposit amount)    
+    ((set! balance (+ balance amount))
+     balance))
+  (define (dispatch m pwd)
+    (if (eq? pwd secret-password)
+        (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unkown request -- MAKE-ACCOUNT" m)))
+        "Incorrect password"))
+  dispatch)
+
+(define acc (make-account 100 'secret-password))
+
+(acc 'withdraw 's)
