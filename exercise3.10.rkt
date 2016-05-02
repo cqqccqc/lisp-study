fc08235f57bbbfbc4f1a2c2fc11330a5dff353f4
陈琦 <cqqccqc@foxmail.com>
2016-05-02 22:44:52 +0800

    exercise3.12

---
+++ exercise3.10.rkt
@@ -0,0 +1,17 @@
+#lang racket
+(define (make-withdraw initial-amount)
+  (let ((balance initial-amount))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds"))))
+
+(define (make-withdraw-lambda initial-amount)
+  ((lambda (balance)
+     (lambda (amount)
+       (if (>= balance amount)
+           (begin (set! balance (- balance amount))
+                  balance)
+           "Insufficient funds"))) initial-amount))
+(cons 'aa 'bb)
---
+++ exercise3.12.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+(define (append! x y)
+  (define (last-pair x)
+    (if (null? (cdr x))
+        x
+        (last-pair (cdr x))))
+  (cons (last-pair x) y))
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+z
+(cdr x)
+
+(define w (append! x y))
+w
+(cdr x)
