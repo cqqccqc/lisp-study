151045da9585dce8a30c15a63dc452d9ea1e2156
cqqccqc <cqqccqc@foxmail.com>
2021-04-08 21:31:54 +0800

    subset2

--- thelittleschemer/c3.rkt
+++ thelittleschemer/c3.rkt
@@ -24,3 +24,41 @@
           (4 5 6)
           (7 8 9)
           ))
+
+
+(define insertR
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          (else
+           (cond ((eq? (car lat) old) (cons old (cons new (cdr lat))))
+                      (else (cons (car lat)(insertR new old (cdr lat)))))))
+    ))
+
+(insertR 10 3 '(1 2 3 4 5 6))
+
+(define insertL
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          (else (cond ((eq? (car lat) old)(cons new lat))
+                      (else (cons (car lat)(insertL new old (cdr lat)))))))
+    ))
+
+(define subset
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          (else (cond ((eq? (car lat) old) (cons new (cdr lat)))
+                      (else (cons (car lat)(subset new old (cdr lat))))))
+          )
+    ))
+
+(subset 10 3 '(1 2 3 4 5 6))
+
+(define subset2
+  (lambda (new o1 o2 lat)
+  (cond ((null? lat) '())
+        (else (cond ((or (eq? (car lat) o1)(eq? (car lat) o2))(cons new (cdr lat)))
+                    (else (cons (car lat)(subset2 new o1 o2 (cdr lat))))
+              )))
+    ))
+
+(subset2 10 5 6 '(1 2 3 4 5 6))
