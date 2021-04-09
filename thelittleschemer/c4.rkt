f3bf5d8f8ceebf03680d30de20f0be54407840a2
cqqccqc <cqqccqc@foxmail.com>
2021-04-09 11:26:25 +0800

    o+

--- thelittleschemer/c4.rkt
+++ thelittleschemer/c4.rkt
@@ -15,3 +15,23 @@
           )
     ))
 (o+ 3 4)
+
+(define o-
+  (lambda (n m)
+    (cond ((zero? m) n)
+          (else (sub1 (o- n (sub1 m)))))))
+(o- 3 2)
+
+(define addtup
+  (lambda (tup)
+    (cond ((null? tup) 0)
+          (else (o+ (car tup)(addtup (cdr tup)))))))
+
+(addtup '(1 2 3 4 5))
+
+(define o*
+  (lambda (n m)
+    (cond ((zero? m) 0)
+          (else (o+ n (o* n (sub1 m)))))))
+(o* 10 20)
+
