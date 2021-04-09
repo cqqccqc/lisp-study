fe55b01d4f6d53918934b10a0a9b625a4168ef7c
cqqccqc <cqqccqc@foxmail.com>
2021-04-09 12:55:11 +0800

    o=

--- thelittleschemer/c4.rkt
+++ thelittleschemer/c4.rkt
@@ -35,3 +35,38 @@
           (else (o+ n (o* n (sub1 m)))))))
 (o* 10 20)
 
+(define tup+
+  (lambda (tup1 tup2)
+    (cond ((null? tup1) tup2)
+          ((null? tup2) tup1)
+          (else
+           (cons (o+ (car tup1)(car tup2))
+                 (tup+ (cdr tup1)(cdr tup2)))))))
+
+(tup+ '(1 2 3) '(3 2 1))
+
+(define o>
+  (lambda (n m)
+    (cond ((zero? m) #t)
+          ((zero? n) #f)
+          (else (o> (sub1 n)(sub1 m)))
+          )))
+(o> 1 2)
+(o> 2 1)
+
+(define o<
+  (lambda (n m)
+    (cond ((zero? m) #f)
+          ((zero? n) #t)
+          (else (o< (sub1 n)(sub1 m)))
+          )))
+(o< 1 2)
+(o< 2 1)
+
+(define o=
+  (lambda (n m)
+    (cond ((o> n m) #f)
+          ((o< n m) #f)
+          (else #t)
+          )))
+(o= 1 1)
