9a915495b97dc271505960d745a620797c77602e
cqqccqc <cqqccqc@foxmail.com>
2021-04-11 21:10:05 +0800

    c5

---
+++ thelittleschemer/c5.rkt
@@ -0,0 +1,34 @@
+#lang racket
+(require "./preset.rkt")
+
+(define rember*
+  (lambda (a l)
+    (cond
+      ((null? l)'())
+      ((atom? (car l))
+       (cond ((eq?(car l)a)
+              (rember* a(cdr l)))
+             (else(cons(car l)
+                       (rember* a(cdr l))))))
+      (else(cons(rember* a(car l))
+                (rember* a(cdr l)))))))
+
+(rember* "cup" '(("coffee")"cup"(("tea")"cup")("and"("hick"))))
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l)'())
+      ((atom? (car l))
+       (cond ((eq?(car l)old)
+              (cons old
+                    (cons new
+                          (insertR* new old
+                                    (cdr l)))))
+             (else (cons (car l)
+                         (insertR* new old
+                                   (cdr l))))))
+      (else (cons (insertR* new old
+                            (car l))
+                  (insertR* new old
+                            (cdr l)))))))
