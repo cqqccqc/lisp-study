cbf8159eb18536b4db5deb412dc9cf7e900a7cc4
cqqccqc <cqqccqc@foxmail.com>
2021-04-18 00:56:15 +0800

    insertL*

--- thelittleschemer/c5.rkt
+++ thelittleschemer/c5.rkt
@@ -58,3 +58,20 @@
                              (subset* new old (cdr l))))))
           (else (cons (subset* new old (car l))
                       (subset* new old (cdr l)))))))
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+      ((null? l)'())
+      ((atom?(car l))
+       (cond ((eq?(car l)old)
+              (cons new
+                    (cons old
+                          (insertL* new old
+                                    (cdr l)))))
+             (else(cons(car l)
+                       (insertL* new old
+                                 (cdr l))))))
+      (else(cons(insertL* new old
+                          (car l))
+                (insertL* new old (cdr l)))))))
