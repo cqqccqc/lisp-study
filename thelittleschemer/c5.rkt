814af51b5e8811eb3e01c6dc7e8c604f42590c87
cqqccqc <cqqccqc@foxmail.com>
2021-05-09 12:33:08 +0800

    rember

--- thelittleschemer/c5.rkt
+++ thelittleschemer/c5.rkt
@@ -92,28 +92,62 @@
       ((atom?(car l))(car l))
       (else(leftmost(car l))))))
 
+;(define eqlist?
+;  (lambda (l1 l2)
+;    (cond ((and(null? l1)(null? l2))#t)
+;          ((or(null? l1)(null? l2))#f)
+;          ((and(atom?(car l1))(atom?(car l2)))
+;           (and(eqan?(car l1)(car l2))
+;               (eqlist?(cdr l1)(cdr l2))))
+;          ((or(atom?(car l1))
+;              (atom?(car l2)))
+;           #f)
+;          (else
+;           (and(eqlist?(car l1)(car l2))
+;               (eqlist?(cdr l1)(cdr l2)))))))
 
-(define eqlist?
-  (lambda (l1 l2)
-    (cond ((and(null? l1)(null? l2))#t)
-          ((or(null? l1)(null? l2))#f)
-          ((and(atom?(car l1))(atom?(car l2)))
-           (and(eqan?(car l1)(car l2))
-               (eqlist?(cdr l1)(cdr l2))))
-          ((or(atom?(car l1))
-              (atom?(car l2)))
-           #f)
-          (else
-           (and(eqlist?(car l1)(car l2))
-               (eqlist?(cdr l1)(cdr l2)))))))
+;(define equal?
+;  (lambda (s1 s2)
+;    (cond
+;      ((and(atom? s1)(atom? s2))
+;       (eqan? s1 s2))
+;      ((atom? s1)#f)
+;      ((atom? s2)#f)
+;      (else(eqlist? s1 s2)))))
 
-(eqlist? '(1 2 3 (3 2) 1) '(1 2 3 (3 2) 1))
+;(eqlist? '(1 2 3 (3 2) 1) '(1 2 3 (3 2) 1))
 
 (define equal?
   (lambda (s1 s2)
     (cond
       ((and(atom? s1)(atom? s2))
        (eqan? s1 s2))
-      ((atom? s1)#f)
-      ((atom? s2)#f)
+      ((or(atom? s1)(atom? s2))
+       #f)
       (else(eqlist? s1 s2)))))
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+      ((and(null? l1)(null? l2))#t)
+      ((or(null? l1)(null? l2))#f)
+      (else
+       (and(equal?(car l1)(car l2))
+           (eqlist?(cdr l1)(cdr l2)))))))
+
+
+;(define rember
+;  (lambda (s l)
+;    (cond
+;      ((null? l)'())
+;      (else (cond
+;              ((equal?(car l)s)(cdr l))
+;              (else(cons(car l)
+;                        (rember s (cdr l)))))))))
+(define rember
+  (lambda (s l)
+    (cond
+      ((null? l)'())
+      ((equal?(car l)s)(cdr l))
+      (else(cons(car l)
+                (rember s (cdr l)))))))
