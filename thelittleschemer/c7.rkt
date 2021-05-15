4ccc98e82ce7b0b560efa61c69f9ce6f80eed607
cqqccqc <cqqccqc@foxmail.com>
2021-05-15 14:55:42 +0800

    intersectall

--- thelittleschemer/c7.rkt
+++ thelittleschemer/c7.rkt
@@ -41,3 +41,54 @@
            (subset(cdr set1)set2))))))
 (subset '(1 2) '(1 2 3))
 
+(define eqset?
+  (lambda(set1 set2)
+    (and(subset? set1 set2)
+        (subset? set2 set1))))
+(eqset? '(1 2) '(1 2))
+
+(define intersect?
+  (lambda(set1 set2)
+    (cond ((null? set1)#f)
+          (else
+           (or(member?(car set1)set2)
+              (intersect?(cdr set1)set2))))))
+
+(define intersect
+  (lambda(set1 set2)
+    (cond
+      ((null? set1)'())
+      ((member?(car set1)set2)
+       (cons(car set1)
+            (intersect(cdr set1)set2)))
+      (else
+       (intersect(cdr set1)set2)))))
+
+(intersect '(1 2 3) '(2 3 4))
+
+(define union
+  (lambda(set1 set2)
+    (cond
+      ((null? set1)set2)
+      ((member?(cdr set1)set2)
+       (union(cdr set1)set2))
+      (else(cons(car set1)
+                (union(cdr set1)set2))))))
+(union '(1 2 3) '(2 3 4))
+
+(define xxx
+  (lambda(set1 set2)
+    (cond
+      ((null? set1)'())
+      ((member?(car set1)set2)
+       (xxx(cdr set1)set2))
+      (else(cons(car set1)
+                (xxx(cdr set1)set2))))))
+
+
+(define intersectall
+  (lambda(l-set)
+    (cond
+      ((null?(cdr l-set))(car l-set))
+      (else(intersect(car l-set)
+                     (intersectall(cdr l-set)))))))
