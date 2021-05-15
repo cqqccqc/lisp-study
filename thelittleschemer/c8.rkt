b988d7b19b445a45b8a581bad00b3a125e6ea7e3
cqqccqc <cqqccqc@foxmail.com>
2021-05-15 16:30:26 +0800

    insert-g

--- thelittleschemer/c8.rkt
+++ thelittleschemer/c8.rkt
@@ -39,3 +39,42 @@
         ((test?(car l)a)(cdr l))
         (else(cons(car l)
                   (rember-f test?) a (cdr l)))))))
+
+(define insertL-f
+  (lambda(test?)
+    (lambda(new old l)
+      (cond
+        ((null? l)'())
+        ((test?(car l)old)
+         (cons new (cons old(cdr l))))
+        (else(cons(car l)
+                  ((insertL-f test?)new old
+                                    (cdr l))))))))
+
+(define insertR-f
+  (lambda(test?)
+    (lambda(new old l)
+      (cond
+        ((null? l)'())
+        ((test?(car l)old)
+         (cons old(cons new (cdr l))))
+        (else(cons(car l)
+                  ((insertR-f test?)new old
+                                    (cdr l))))))))
+
+(define seqL
+  (lambda(new old l)
+    (cons new(cons old l))))
+(define seqR
+  (lambda(new old l)
+    (cons old(cons new l))))
+
+(define insert-g
+  (lambda(seq)
+    (lambda(new old l)
+      (cond ((null? l)'())
+            ((eq?(car l)old)
+             (seq new old (cdr l)))
+            (else(cons(car l)
+                      ((insert-g seq)new old
+                                     (cdr l))))))))
